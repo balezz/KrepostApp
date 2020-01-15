5756f8f4fd7d90b862783cfafd7174d6a6b528d6
balezz <balib@rambler.ru>
2020-01-15 19:26:54 +0300

    Delete Single fragment code, prepare to include Navigation UI

--- app/src/main/java/ru/balezz/krepostapp/MainActivity.java
+++ app/src/main/java/ru/balezz/krepostapp/MainActivity.java
@@ -12,23 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment sensor_fragment = fm.findFragmentById(R.id.sensor_fragment_id);
-        Fragment scheme_fragment = fm.findFragmentById(R.id.scheme_fragment_id);
-
-        if (sensor_fragment == null) {
-            sensor_fragment = new SensorFragment();
-            fm.beginTransaction()
-                    .add(R.id.sensor_fragment_id, sensor_fragment)
-                    .commit();
-        }
-
-        if (scheme_fragment == null) {
-            scheme_fragment = new SchemeFragment();
-            fm.beginTransaction()
-                    .add(R.id.scheme_fragment_id, scheme_fragment)
-                    .commit();
-        }
     }
 }
