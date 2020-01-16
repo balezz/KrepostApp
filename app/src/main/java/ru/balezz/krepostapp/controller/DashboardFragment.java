e88313fcfd679319bdb1c09b4ffdad88496ee289
balezz <balib@rambler.ru>
2020-01-16 09:08:21 +0300

    Add sensors and notify recycler views

--- app/src/main/java/ru/balezz/krepostapp/controller/DashboardFragment.java
+++ app/src/main/java/ru/balezz/krepostapp/controller/DashboardFragment.java
@@ -8,14 +8,63 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import ru.balezz.krepostapp.R;
+import ru.balezz.krepostapp.model.SecuritySensor;
+import ru.balezz.krepostapp.model.SensorLab;
 
 public class DashboardFragment extends Fragment {
+
+    RecyclerView mSensorsRecyclerView;
+
+    private class SensorHolder extends RecyclerView.ViewHolder {
+        public SensorHolder(LayoutInflater inflater, @NonNull ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_sensor, parent, false));
+        }
+    }
+
+    private class SensorAdapter extends RecyclerView.Adapter<SensorHolder> {
+        List<SecuritySensor> mSensors;
+
+        public SensorAdapter(List<SecuritySensor> sensors) {
+            mSensors = sensors;
+        }
+
+        @NonNull
+        @Override
+        public SensorHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            return new SensorHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SensorHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSensors.size();
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mSensorsRecyclerView = root.findViewById(R.id.sensors_recyclerview);
+        mSensorsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
+        return root;
+    }
+
+    private void updateUI() {
+        List<SecuritySensor> sensors = SensorLab.getInstance(getActivity()).getSensors();
+        SensorAdapter adapter = new SensorAdapter(sensors);
+        mSensorsRecyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/ru/balezz/krepostapp/controller/NotificationsFragment.java
+++ app/src/main/java/ru/balezz/krepostapp/controller/NotificationsFragment.java
@@ -7,15 +7,65 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import ru.balezz.krepostapp.R;
+import ru.balezz.krepostapp.model.Notify;
+import ru.balezz.krepostapp.model.NotifyLab;
 
 public class NotificationsFragment extends Fragment {
 
+    RecyclerView mRecyclerView;
+
+    private class NotifyViewHolder extends RecyclerView.ViewHolder {
+
+        public NotifyViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_notify, parent, false));
+        }
+    }
+
+    private class NotifyAdapter extends RecyclerView.Adapter<NotifyViewHolder> {
+        List<Notify> mNotifies;
+
+        public NotifyAdapter(List<Notify> notifies) {
+            mNotifies = notifies;
+        }
+
+        @NonNull
+        @Override
+        public NotifyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            return new NotifyViewHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NotifyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNotifies.size();
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        mRecyclerView = root.findViewById(R.id.notify_recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
         return root;
     }
+
+    private void updateUI() {
+        List<Notify> notifies = NotifyLab.getInstance(getActivity()).getNotifies();
+        NotifyAdapter adapter = new NotifyAdapter(notifies);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+
 }
